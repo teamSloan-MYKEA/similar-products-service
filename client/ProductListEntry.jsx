17e7f919ce227b4101f5b8dacc2586a451e2c238
Hewah Bahrami <hewbahrami@gmail.com>
2020-08-13 11:33:51 -0400

    fills in heart on click

--- client/ProductListEntry.jsx
+++ client/ProductListEntry.jsx
@@ -5,6 +5,10 @@ import { faHeart as heartRegular } from '@fortawesome/free-regular-svg-icons';
 import { faHeart as heartSolid } from '@fortawesome/free-solid-svg-icons';
 import styled from 'styled-components';
 
+const MainProductContainer = styled.div`
+
+`;
+
 const Product = styled.div`
   @import url('https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap');
   @import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@700&display=swap');
@@ -14,6 +18,9 @@ const Product = styled.div`
   width: 20vw;
   position:relative;
   margin: 1vw;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
 `;
 const ProductName = styled.div`
   line-height: 1.42857;
@@ -57,8 +64,7 @@ class ProductListEntry extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showHeart: true,
-      showBag: false,
+      showHeart: false,
       like: false,
     };
     this.onImageHover = this.onImageHover.bind(this);
@@ -66,20 +72,22 @@ class ProductListEntry extends React.Component {
   }
 
   onImageHover() {
-    const { like, showHeart, showBag } = this.state;
-    if (like) {
-      this.setState({
-        showHeart: true,
-      });
-    } else {
+    const { like, showHeart } = this.state;
+    // if (like) {
       this.setState({
         showHeart: !showHeart,
-        showBag: !showBag,
-      });
-    }
+      // });
+    })
+    // else {
+    //   this.setState({
+    //     showHeart: !showHeart,
+    //     showBag: !showBag,
+    //   });
+    // }
   }
 
   onHeartClick() {
+    console.log('clicked')
     const { like } = this.state;
     const { onLikeBagClick, product } = this.props;
     const { name } = product;
@@ -93,33 +101,41 @@ class ProductListEntry extends React.Component {
     const {
       photo1, photo2, name, description, price,
     } = this.props.product;
-    const { showHeart, showBag, like } = this.state;
+    const { showHeart, like } = this.state;
     return (
-      <Product >
+      <MainProductContainer onMouseEnter={this.onImageHover} onMouseLeave={this.onImageHover}>
         {showHeart
           ? (
             <FontAwesomeIcon
               icon={like ? heartSolid : heartRegular}
               onClick={this.onHeartClick}
-              style={HeartStyle}
+              style={{
+              justifyContent: 'right',
+              alignItems: 'right',
+              transform: 'scale(1.2)',
+              height: '40px',
+              visibility: showHeart ? 'visible' : 'hidden',
+              transition: 'visibility 3s linear'}}
             />
           )
           : <EmptyDiv />}
-        <Image
-          src={photo1}
-          onMouseOver={(e) => (e.target.src = photo2)}
-          onMouseOut={(e) => (e.target.src = photo1)}
-        />
-        <ProductName>{name}</ProductName>
-        <ProductDescription>{description}</ProductDescription>
-        <ProductPrice>
-          <PriceDetails style={{ marginLeft: '-8px' }}>$</PriceDetails>
-          {price}
-          <PriceDetails>.00</PriceDetails>
-        </ProductPrice>
-        <div className="product-stars">stars</div>
-        <MoreOptions>more options</MoreOptions>
-      </Product>
+        <Product>
+          <Image
+            src={photo1}
+            onMouseOver={(e) => (e.target.src = photo2)}
+            onMouseOut={(e) => (e.target.src = photo1)}
+          />
+          <ProductName style={{ textDecoration: showHeart ? 'underline' : 'none' }}>{name}</ProductName>
+          <ProductDescription>{description}</ProductDescription>
+          <ProductPrice>
+            <PriceDetails style={{ marginLeft: '-8px' }}>$</PriceDetails>
+            {price}
+            <PriceDetails>.00</PriceDetails>
+          </ProductPrice>
+          <div className="product-stars">stars</div>
+          <MoreOptions>more options</MoreOptions>
+        </Product>
+      </MainProductContainer>
     );
   }
 }
