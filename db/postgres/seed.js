7f27128413dbdcd48bf26b3a8e6b5947a40f2a5e
Nathan Vu <mr.nathanvu@gmail.com>
2020-09-02 10:29:06 -0700

    importing 10M data into Docker'Postgres & Cassandra works

--- db/postgres/seed.js
+++ db/postgres/seed.js
@@ -8,20 +8,20 @@ const client = new Client({
   database: 'hrr47mykea',
 });
 
-const schema = `
+const query = `
 DROP TABLE IF EXISTS mykea_similarproducts;
 CREATE TABLE mykea_similarproducts (
-  ID    SERIAL PRIMARY KEY  NOT NULL,
-  PRODUCTID    INT          NOT NULL,
-  NAME         VARCHAR(25)  NOT NULL,
-  DESCRIPTION  VARCHAR(25)  NOT NULL,
-  STARS        INT          NOT NULL,
-  PRICE        INT          NOT NULL,
-  PHOTO1       TEXT         NOT NULL,
-  PHOTO2       TEXT         NOT NULL
+  id    SERIAL PRIMARY KEY  NOT NULL,
+  productid    INT          NOT NULL,
+  name         VARCHAR(25)  NOT NULL,
+  description  VARCHAR(25)  NOT NULL,
+  stars        INT          NOT NULL,
+  price        INT          NOT NULL,
+  photo1       TEXT         NOT NULL,
+  photo2       TEXT         NOT NULL
 );
-COPY mykea_similarproducts(PRODUCTID, NAME, DESCRIPTION, STARS, PRICE, PHOTO1, PHOTO2)
-FROM '/var/lib/postgresql/data/records.csv'
+COPY mykea_similarproducts(productid, name, description, stars, price, photo1, photo2)
+FROM '/var/lib/postgresql/data/postgres_records.csv'
 DELIMITER ','
 CSV HEADER;
 `;
@@ -31,7 +31,7 @@ client.connect(err => {
     console.log('POSTGRES CONNECTING FAIL', err.stack);
   } else {
     console.log('POSTGRES CONNECTING SUCCESS');
-    client.query(schema, (err, res) => {
+    client.query(query, (err, res) => {
       if (err) {
         console.log('CREATING TABLE FAIL', err.stack);
       } else {
@@ -47,12 +47,18 @@ client.connect(err => {
   }
 });
 
-// -------------------- DOCKER COMMAND LINES --------------------
+/* -------------------- DOCKER COMMAND LINES --------------------
 
-// docker run --name postgresSDC -p 5432:5432 -v /Users/nathanvu/Downloads/zHackReactor/Github/SDC/similar-products-service/db/csv-generator:/var/lib/postgresql/data -e POSTGRES_PASSWORD=mysecretpassword -e POSTGRES_DB=hrr47mykea -e PGDATA=/var/lib/postgresql/data/db-files/  -d postgres
+docker run --name postgresSDC -p 5432:5432 -v /Users/nathanvu/Downloads/zHackReactor/Github/SDC/similar-products-service/db/csv-generator:/var/lib/postgresql/data -e POSTGRES_PASSWORD=mysecretpassword -e POSTGRES_DB=hrr47mykea -e PGDATA=/var/lib/postgresql/data/db-files/  -d postgres
 
-// docker exec -it postgresSDC bash
+docker exec -it postgresSDC bash
 
-// psql -U postgres
+psql -U postgres
 
-// select * from mykea_similarproducts;
+\l
+\c hrr47mykea
+SELECT * FROM mykea_similarproducts;
+(DROP TABLE IF EXISTS mykea_similarproducts;)
+(SELECT * FROM mykea_similarproducts WHERE id=10000000;)
+
+*/
